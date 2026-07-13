c4d73527248e7640209141c1230950dff589736b
Kenji <bruno_kny@hotmail.com>
2026-07-13 00:56:27 -0300

    adicionado metodo post

--- src/main/java/com/javanauta/agendadortarefas/infrastruture/client/UsuarioClient.java
+++ src/main/java/com/javanauta/agendadortarefas/infrastruture/client/UsuarioClient.java
@@ -3,6 +3,7 @@ package com.javanauta.agendadortarefas.infrastruture.client;
 import com.javanauta.agendadortarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient (name = "usuario", url = "${usuario.url}")
@@ -10,6 +11,6 @@ public interface UsuarioClient {
 
     @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail (@RequestParam("email") String email,
-                                     @RequestParam("Authorization") String token);
+                                     @RequestHeader("Authorization") String token);
 
 }
